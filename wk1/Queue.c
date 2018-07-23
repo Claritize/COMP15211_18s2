fc47321a5794798fc6013274fffe6579a8432f34
Anna Zhang <annazhang343@gmail.com>
2018-07-23 16:24:39 +1000

    wk1 updated

---
+++ wk1/Queue.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Queue.h"
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
+
+// initialise a Queue
+void initQueue(Queue *q) {
+
+}
+
+// insert a new item at the tail of the Queue
+void  enterQueue(Queue *, int) {
+
+}
+
+// remove/return the item at the head of the Queue
+int   leaveQueue(Queue *) {
+
+}
+
+// return the number of items currently in the Queue
+int   lengthQueue(Queue) {
+
+}
+
+// display the contents of the Queue
+void showQueue(Queue) {
+
+}
+ 
