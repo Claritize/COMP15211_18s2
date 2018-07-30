0bc5c1626f4bfec8e21c3a4967e38ba3a75f20df
Anna Zhang <annazhang343@gmail.com>
2018-07-30 19:25:00 +1000

    Added week 2 content

---
+++ wk2/BitShift.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int Word;
+
+Word reverseBits(Word w);
+
+int main(void) {
+
+    Word w1 = 0x01234567;
+    //0000 => 0000 = 0
+    //0001 => 1000 = 8
+    //0010 => 0100 = 4
+    //0011 => 1100 = C
+    //0100 => 0010 = 2
+    //0101 => 1010 = A
+    //0110 => 0110 = 6
+    //0111 => 1110 = E
+    assert(reverseBits(w1) == 0xE6A2C480);
+    printf("All tests passed!");
+
+    return 0;
+}
+
+Word reverseBits(Word w) {
+
+    
+}
